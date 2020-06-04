e0009dc60592f7653148886dd66e739b4e3bf745
satinderthakur <59055953+satinderthakur@users.noreply.github.com>
2020-06-04 16:55:47 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -12,8 +12,8 @@ resource "aws_security_group" "my-web-ssh-sg" {
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = 0
+    to_port = 0
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
