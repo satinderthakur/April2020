fb54c1635281f1a602532219575cb0b801006a03
satinderthakur <59055953+satinderthakur@users.noreply.github.com>
2020-06-06 23:25:54 +0530

    run the container

--- instance.tf
+++ instance.tf
@@ -13,7 +13,6 @@ tags = {
              #!/bin/bash
              yum update -y
              yum install httpd -y
-             echo "Hello World" > /var/www/html/index.html
              service httpd start
              service httpd on
              sudo yum update -y
