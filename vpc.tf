d2473354fb9cb0f02531077d80c742addcb32e58
satinderthakur <59055953+satinderthakur@users.noreply.github.com>
2020-06-06 22:35:07 +0530

    commit the public and private subnet

--- vpc.tf
+++ vpc.tf
@@ -9,4 +9,40 @@ resource "aws_vpc" "myVPC"{
     Name = "myVPC"
   }
 }
-
+# Public 
+resource "aws_subnet" "my-public-sub-1"{
+   vpc_id = aws_vpc.myVPC.id
+   cidr_block = "10.0.1.0/24"
+   map_public_ip_on_launch = "true"
+   availability_zone = "${var.AWS_DEFAULT_REGION}a"
+   tags = {
+       Name = "my-public-sub-1"
+   }
+}
+resource "aws_subnet" "my-public-sub-2"{
+   vpc_id = aws_vpc.myVPC.id
+   cidr_block = "10.0.2.0/24"
+   map_public_ip_on_launch = "true"
+   availability_zone = "${var.AWS_DEFAULT_REGION}b"
+   tags = {
+       Name = "my-public-sub-2"
+   }
+}
+resource "aws_subnet" "my-private-sub-1"{
+   vpc_id = aws_vpc.myVPC.id
+   cidr_block = "10.0.4.0/24"
+   map_public_ip_on_launch = "false"
+   availability_zone = "${var.AWS_DEFAULT_REGION}a"
+   tags = {
+       Name = "my-private-sub-1"
+   }
+}
+resource "aws_subnet" "my-private-sub-2"{
+   vpc_id = aws_vpc.myVPC.id
+   cidr_block = "10.0.5.0/24"
+   map_public_ip_on_launch = "false"
+   availability_zone = "${var.AWS_DEFAULT_REGION}b"
+   tags = {
+       Name = "my-private-sub-2"
+   }
+}
